f1174c1c8a64e58064c7486b0874a692acfe5e1b
Artem_Lakatun <alakatuni@mail.ru>
2023-12-25 20:42:18 +0300

    feat: create header

--- additional_pages/admin.php
+++ additional_pages/admin.php
@@ -1,1 +1,37 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kalnia:wght@500&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+<header class="header">
+    <div class="logo">
+        <p>training PHP</p>
+    </div>
+    <div class="nav-bar">
+        <ul class="nav-menu">
+            <a class="link" href="../index.php">main page</a>
+        </ul>
+    </div>
+</header>
+<section class="container">
+    <h2>Admin panel</h2>
+</section>
+<footer class="footer">
+    <p>2023</p>
+</footer>
+</body>
+</html>
