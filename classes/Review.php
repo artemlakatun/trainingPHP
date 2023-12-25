cd46931048cfb2e4eb50dd56e422a0095e26c60a
Artem_Lakatun <alakatuni@mail.ru>
2023-12-25 20:39:01 +0300

    feat: add class Review

--- classes/Review.php
+++ classes/Review.php
@@ -1,1 +1,15 @@
 <?php
+
+class Review {
+    public $user_name;
+    public $subject;
+    public $text;
+    public $evaluation;
+
+    public function __construct ($user_name, $subject, $text, $evaluation) {
+        $this ->user_name = $user_name;
+        $this ->subject = $subject;
+        $this ->text = $text;
+        $this ->evaluation = $evaluation;
+    }
+}
