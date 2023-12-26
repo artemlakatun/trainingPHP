c0e1229e196f45db9150a624bdd531330c8fbc01
Artem_Lakatun <alakatuni@mail.ru>
2023-12-26 17:02:10 +0300

    feat: add ID

--- request/post_review.php
+++ request/post_review.php
@@ -2,17 +2,17 @@
 require_once '../include_db/db.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Получение данных из формы
+    $id = $_POST['id'];
     $user_name = $_POST['user_name'];
     $subject = $_POST['subject'];
     $text = $_POST['text'];
     $evaluation = $_POST['evaluation'];
 
-    $sql = "INSERT INTO review (user_name, subject, text, evaluation) VALUES (?, ?, ?, ?)";
+    $sql = "INSERT INTO review (id, user_name, subject, text, evaluation) VALUES (?, ?, ?, ?, ?)";
 
     /** @var TYPE_NAME $pdo */ //костыль!
     $stmt = $pdo->prepare($sql);
-    $stmt->execute([$user_name, $subject, $text, $evaluation]);
+    $stmt->execute([$id, $user_name, $subject, $text, $evaluation]);
     header('Location: ../index.php');
 } else {
     header('Location: error.php');
