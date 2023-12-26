f41864e51109b7740fce08ac20500914ebd2b12c
Artem_Lakatun <alakatuni@mail.ru>
2023-12-26 17:01:15 +0300

    feat: added forms for delete and change requests

--- additional_pages/admin.php
+++ additional_pages/admin.php
@@ -1,3 +1,24 @@
+<?php
+require_once '../include_db/db.php';
+require_once '../classes/Review.php';
+
+/** @var TYPE_NAME $pdo */ //костыль!
+$stmt = $pdo->query('SELECT id, user_name, subject, text, evaluation FROM review');
+$reviewsData = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// Создаем экземпляры класса Review для каждого отзыва
+$reviews = [];
+foreach ($reviewsData as $reviewData) {
+    $review = new Review(
+        $reviewData['id'],
+        $reviewData['user_name'],
+        $reviewData['subject'],
+        $reviewData['text'],
+        $reviewData['evaluation']
+    );
+    $reviews[] = $review;
+}
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -28,10 +49,32 @@
     </div>
 </header>
 <section class="container">
-    <h2>Admin panel</h2>
+    <?php
+    foreach ($reviews as $review):
+    ?>
+        <div class="review">
+            <div class="user-name review-column"><strong>User:</strong> <?= $review->user_name ?></div>
+            <div class="subject review-column"><strong>Subject:</strong> <?= $review->subject ?></div>
+            <div class="text review-column"><?= $review->text ?></div>
+            <div class="evaluation review-column"><strong>evaluation:</strong> <?= $review->evaluation ?></div>
+
+            <form action="../request/edit_review.php" method="post">
+                <input type="hidden" name="id" value="<?= $review->id ?>">
+                <input class="user-name review-column" type="text" name="user_name" value="<?= $review->user_name ?>">
+                <input class="subject review-column" type="text" name="subject" value="<?= $review->subject ?>">
+                <input class="text review-column" type="text" name="text" value="<?= $review->text ?>">
+                <input class="evaluation review-column" type="text" name="evaluation" value="<?= $review->evaluation ?>">
+                <input class="edit" type="submit" value="Сохранить изменения">
+            </form>
+
+            <form class="delete-form" action="../request/delete_review.php" method="post">
+                <input type="hidden" name="id" value="<?= $review->id ?>">
+                <input type="submit" value="Удалить">
+            </form>
+        </div>
+    <?php
+    endforeach;
+    ?>
 </section>
-<footer class="footer">
-    <p>2023</p>
-</footer>
 </body>
 </html>
