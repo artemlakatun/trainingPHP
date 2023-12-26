fc84bf90b0ce86cbccc9c384615b6c440c9fc894
Artem_Lakatun <alakatuni@mail.ru>
2023-12-26 16:59:53 +0300

    feat: add ID in constructor

--- classes/Review.php
+++ classes/Review.php
@@ -1,15 +1,17 @@
 <?php
 
 class Review {
+    public $id;
     public $user_name;
     public $subject;
     public $text;
     public $evaluation;
 
-    public function __construct ($user_name, $subject, $text, $evaluation) {
-        $this ->user_name = $user_name;
-        $this ->subject = $subject;
-        $this ->text = $text;
-        $this ->evaluation = $evaluation;
+    public function __construct ($id, $user_name, $subject, $text, $evaluation) {
+        $this -> id = $id;
+        $this -> user_name = $user_name;
+        $this -> subject = $subject;
+        $this -> text = $text;
+        $this -> evaluation = $evaluation;
     }
 }
