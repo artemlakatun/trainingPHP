9b0e869d5455c75e7c6bc000c21f74afd79e9104
Artem_Lakatun <alakatuni@mail.ru>
2024-01-08 20:25:11 +0300

    feat:  file was made on a request error

---
+++ additional_pages/error.php
@@ -0,0 +1,5 @@
+<?php
+require_once "../blocks/head.php";
+?>
+<h5 class="error-h5">Ops, something went wrong, sorry!</h5>
+<p class="error-p-link"><a class="error-link" href="#">back to main page</a></p>
